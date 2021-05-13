7d1132ea89ecef24449618c38bfdabd1c7258a0e
jude akinwale <judeakinwale@gmail.com>
2021-05-13 23:15:58 +0100

    Minor spacing changes

--- app/core/tests/test_command.py
+++ app/core/tests/test_command.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 
 
 class CommandTests(TestCase):
-    
+
     def test_wait_for_db_ready(self):
         """test waiting for the database when it is available"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
